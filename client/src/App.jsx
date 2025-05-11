45fde16524583fdbdec382f3f64bc6800187f66a
Habib Hossain <habibfahim71@gmail.com>
2025-05-11 18:48:40 +0600

    Live-Chat project setup are done  some setup are remain

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold text-orange-500 text-shadow-2xs">This is Tailwind Css</h1>
+    </div>
+  );
+};
+
+export default App;
